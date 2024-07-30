319fbd19586e9d0a28988fb6a8f91a4eab4d9572
iYoonJongWon <gksso25@gmail.com>
2024-07-30 13:37:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 # from dotenv import load_dotenv
 # load_dotenv()
 import streamlit as st
-from langchain_openai import ChatOpenAI
+from langchain.chat_models import ChatOpenAI
 
-chat_model = ChatOpenAI()
+chat_model = ChatOpenAI(model = "GPT-3.5 Turbo Chatbot")
 
 st.title('AI 작사가')
 
 content = st.text_input('작사할 주제를 제시해주세요。')
 
 if st.button('작사 요청하기'):
-    with st.spinner('시를 작성 중입니다...'):
-        result = chat_model.invoke(content + "에 대한 노래의 작사를 해 줘")
-        st.write(result.content)
+    result = chat_model.invoke(content + "에 대한 노래의 작사를 해 줘")
+    st.write(result.content)
